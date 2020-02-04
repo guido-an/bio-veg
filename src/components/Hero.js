f3aa4964cd27fbdc117f423c2df67ff94414b0bc
Guido <carucciguido@gmail.com>
2020-02-04 15:56:02 +0100

    hero and header

---
+++ src/components/Hero.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const Hero = () => (
+
+  <section className='hero'>
+    <div className='hero-wrapper'>
+      <h1>
+        lorem <span>ipsum</span> doloret
+      </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      <Link className='hero-btn' id='btn1' to='/paccasassi'>PACCASASSI</Link>
+      <Link className='hero-btn' id='btn2' to='/contatti'>CONTATTI</Link>
+    </div>
+    <style jsx='true'>
+      {`
+        .hero {
+          text-align: center;
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+        }
+        .hero-wrapper{
+          margin-top: 80px
+        }
+        .hero-wrapper h1 {
+          text-transform: uppercase;
+          padding: 0;
+          margin: 0;
+          font-size: 32px;
+          color: #241f20
+        }
+        .hero-wrapper span {
+          color: #23662e;
+        }
+        .hero-wrapper p {
+          font-weight: 500;
+          font-size: 16px;
+          position: relative;
+          bottom: 0px;
+          color: #777
+        }
+        .hero-btn{
+          text-decoration: none;
+          border-radius: 4px;
+          padding: 10px 15px;
+          display: inline-block;
+          width: 120px;
+          margin: 25px 10px;
+          font-size: 12px;
+          letter-spacing: 0.4px;
+          font-weight: 500
+        }
+        #btn1{
+          border: 2px solid #241f20;
+          color: #241f20 !important;
+        }
+        #btn2{
+          background-color:#23662e;
+          border: 2px solid #23662e;
+          color: #fff;
+        }
+        #btn1:hover{
+          border: 2px solid #23662e;
+          color: #23662e !important;
+        }
+        
+        #btn2:hover{
+          background-color:#241f20;
+          border: 2px solid #241f20;
+        }
+       
+        @media (min-width: 1200px) {
+          .hero-wrapper h1 {
+            font-size: 52px;
+          }
+          .hero-wrapper p {
+             font-size: 18px;
+          }
+        }
+      `}
+    </style>
+  </section>
+
+)
+
+export default Hero
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,157 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import { Link } from "gatsby"
+import logo from "../images/bio-veg-logo.png"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+class Header extends React.Component {
+  state = {
+    visible: false,
+  }
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  showMobileMenu = () => {
+    this.setState(prevState => {
+      return { visible: !prevState.visible }
+    })
+  }
 
-Header.defaultProps = {
-  siteTitle: ``,
+  render() {
+    return (
+      <header>
+        <section className="mobile-nav">
+          <Link to="/">
+            {" "}
+            <img src={logo} alt="logo-bio-veg" width="120px" />
+          </Link>
+          {this.state.visible ? (
+            <div onClick={this.showMobileMenu} className="burger-icon-close">
+              <span>X</span>
+            </div>
+          ) : (
+            <div onClick={this.showMobileMenu} className="burger-icon-open">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          )}
+        </section>
+
+        {this.state.visible ? (
+          <nav className="mobile-menu header">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/progetto">Progetto</Link>
+              </li>
+              <li>
+                <Link to="/paccasassi">Paccasassi</Link>
+              </li>
+              <li>
+                <Link to="/team">Partners</Link>
+              </li>
+              <li>
+                <Link to="/contatti">Contatti</Link>
+              </li>
+            </ul>
+          </nav>
+        ) : (
+          <p></p>
+        )}
+
+        <nav className="desktop-menu header">
+          <Link to="/">
+            {" "}
+            <img className="logo" src={logo} alt="logo-bio-veg" width="140px" />
+          </Link>
+          <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/progetto">Progetto</Link>
+              </li>
+              <li>
+                <Link to="/paccasassi">Paccasassi</Link>
+              </li>
+              <li>
+                <Link to="/team">Partners</Link>
+              </li>
+              <li>
+                <Link to="/contatti">Contatti</Link>
+              </li>
+            </ul>
+        </nav>
+
+        <style jsx="true">{`
+          .mobile-nav {
+            display: flex;
+            justify-content: space-between;
+            padding: 20px;
+          }
+          .header li  {
+            text-decoration: none;
+            list-style: none
+          }
+          .header li a{
+            text-decoration: none;
+            color: #777
+          }
+          .desktop-menu {
+            display: none;
+          }
+          .header a:hover {
+            color: #241f20 !important;
+          }
+          .burger-icon-close span {
+            font-size: 26px;
+            color: #777;
+            position: relative;
+            top: 50px;
+            right: 20px;
+          }
+          .burger-icon-open {
+            position: relative;
+            top: 55px;
+            right: 10px;
+          }
+          .burger-icon-open span {
+            width: 25px;
+            border: 1px solid #777;
+            margin: 6px 0;
+            display: block;
+          }
+          .mobile-menu li {
+            margin: 20px 0;
+          }
+          @media (min-width: 1200px) {
+            .mobile-nav {
+              display: none;
+            }
+            .mobile-menu {
+              display: none;
+            }
+            .logo{
+              position: relative;
+              bottom: 10px
+            }
+            .desktop-menu {
+              display: flex;
+              justify-content: space-around;
+              margin-top: 20px
+            }
+            .desktop-menu ul {
+              display: flex;
+              position: relative;
+              top: 10px
+            }
+            .desktop-menu ul li {
+              margin: 20px;
+            }  
+          }
+        `}</style>
+      </header>
+    )
+  }
 }
 
 export default Header
