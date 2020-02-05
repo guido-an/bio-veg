606e30fd26e79ab868a257c4952d817f4c154945
Guido <carucciguido@gmail.com>
2020-02-05 18:09:44 +0100

    first deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
-// import Hero from '../components/hero'
+import Hero from '../components/hero'
 import SEO from '../components/seo'
 import ServicesHome from '../components/servicesHome'
 import PartnersHome from '../components/partnersHome'
@@ -9,6 +9,7 @@ import ContactCta from '../components/contactCta'
 const IndexPage = () => (
   <Layout>
     <SEO title='Bio Veg' />
+    <Hero />
     <ServicesHome />
     <PartnersHome />
     <ContactCta />
