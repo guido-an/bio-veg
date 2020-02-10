71fb32a164f12c04bd171f241d33c44e3c25872c
Guido <carucciguido@gmail.com>
2020-02-10 14:16:45 +0100

    privacy

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,8 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import CookieConsent from "react-cookie-consent";
 
 import Header from './header'
 import Footer from './footer'
@@ -29,6 +30,17 @@ const Layout = ({ children }) => {
       <Header />
       <main>{children}</main>
       <Footer />
+      <CookieConsent
+    location="bottom"
+    buttonText="Ok!"
+    cookieName="myAwesomeCookieName2"
+    style={{ background: "#2D672E" }}
+    buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+    expires={150}
+>
+    Questo sito utilizza i cookie per migliorare l'esperienza dell'utente.  {" "}
+    <Link style={{color: '#fff'}} to="/policy">Policy</Link>
+</CookieConsent>
     </>
   )
 }
