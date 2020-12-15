8ad31777ca308a6626c911c4f1fd29d062e31725
Guido <carucciguido@gmail.com>
2020-12-15 12:25:33 +0000

    new event

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -62,6 +62,7 @@ const Hero = () => (
         }
       `}
     </style>
+    
   </section>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Seo from '../components/seo'
 import ServicesHome from '../components/servicesHome'
 import PartnersHome from '../components/partnersHome'
 import ContactCta from '../components/contactCta'
+import img from '../images/seminario.jpg'
 
 const IndexPage = () => (
   <Layout>
@@ -13,6 +14,18 @@ const IndexPage = () => (
       description="Diffondiamo l'utilizzo del finocchio marino del Conero attraverso la creazione di conserve biologiche e la sperimentazione di tecniche agricole."
     />
     <Hero />
+    <div>
+      <div style={{
+        textAlign: 'center',
+        position: 'relative',
+        bottom: '80px'
+      }}
+      >
+        <p><strong>"PACCASASSI: LE NUOVE CONSERVE BIO" - SEMINARIO ONLINE IL 18 DICEMBRE 2020</strong></p>
+        <p>Per partecipare occorre collegarsi alla piattaforma Zoom a questo <a href='https://zoom.us/j/93004939829'>link</a></p>
+        <img src={img} />
+      </div>
+    </div>
     <ServicesHome />
     <PartnersHome />
     <ContactCta />
